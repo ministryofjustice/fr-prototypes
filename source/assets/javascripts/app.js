1fc9845d72a256c201078c06bc991d1099fcb658
axemonkey <hi+github@clivemurray.com>
2016-05-10 14:52:00 +0100

    branch savings question in staff prototype

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -44,9 +44,13 @@ function getUrlParameter(sParam) {
 }
 
 function listLocalStorage() {
-  var obj = {};
-  for (var i = 0; i < localStorage.length; i++){
-    obj[localStorage.key(i)] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+  var obj = {},
+      app = document.location.href.indexOf('/staff/') === -1 ? 'public' : 'staff';
+
+  for (var i = 0; i < localStorage.length; i++) {
+    if(localStorage.key(i).indexOf(app) === 0) {
+      obj[localStorage.key(i)] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    }
   }
 
   console.log(obj);
@@ -70,12 +74,14 @@ function getValue(app, key) {
     return val;
   }
 
-  console.log('nope')
+  console.log('nope');
   return false;
 }
 
 function clearValue(app, key) {
   localStorage.removeItem(app + '_' + key);
+
+  console.log('removed: ' + app + '_' + key);
 }
 
 function clearAppData(app) {
@@ -87,4 +93,38 @@ function clearAppData(app) {
       localStorage.removeItem(key);
     }
   }
+}
+
+function calcThreshold() {
+  var threshold = 3000,
+      bands = [
+        [1000, 4000],
+        [1335, 5000],
+        [1665, 6000],
+        [2000, 7000],
+        [2330, 8000],
+        [4000, 10000],
+        [5000, 12000],
+        [6000, 14000],
+        [7000, 16000]
+      ],
+      fee = getValue('staff', 'fee'),
+      age = getValue('staff', 'age');
+
+  if(age && age > 60) {
+    threshold = 16000;
+  } else if(fee) {
+    for(x in bands) {
+      if(fee > bands[x][0]) {
+        threshold = bands[x][1];
+      }
+    }
+  }
+  return threshold;
+}
+
+function deselectRadio(els) {
+  els.forEach(function(el) {
+    $(el).prop('checked', false).closest('label').removeClass('selected');
+  });
 }

abca19c1bd89e1425fbef74821a40138f1439f38
axemonkey <hi+github@clivemurray.com>
2016-05-20 16:40:32 +0100

    generate income range from status and num children

--- source/assets/javascripts/income_threshold.js
+++ source/assets/javascripts/income_threshold.js
@@ -3,15 +3,31 @@
 var incomeThresholdModule = {
   prefixes: ['user', 'partner'],
   isMarried: null,
+  baseThresholdFloor: 1085,
+  thresholdRange: 4000,
+  thresholdMarriedAllowance: 160,
+  thresholdChildAllowance: 245,
 
   init: function() {
     var self = this,
-        incomeLists = $('ul.income-source-list');
+        $form = $('form#income-amount'),
+        incomeLists = $('ul.income-sources', $form);
 
     if(incomeLists.length) {
       self.isMarried = getValue('public', 'married').toString();
+      self.updateThresholdFigures();
       self.writeIncomeList(incomeLists);
+
+      self.bindEvents();
     }
+  },
+
+  bindEvents: function() {
+    $('[name="income-amount"]').on('change', function(e) {
+      var $el = $(e.target);
+
+      console.log($el.val());
+    });
   },
 
   writeIncomeList: function(incomeLists) {
@@ -19,17 +35,41 @@ var incomeThresholdModule = {
         sources = getValue('public', 'income-sources'),
         html = '';
 
-    self.prefixes.forEach(function(prefix) {
-      sources.forEach(function(source) {
-        if(source[prefix].toString() === 'true') {
-          var prefixText = (prefix === 'user' ? 'You' : 'Your partner');
-          html += '<li>' + prefixText + ': ' + source.text + '</li>';
+    sources.forEach(function(source) {
+      var entry = '';
+      self.prefixes.forEach(function(prefix) {
+        if(source[prefix] && source[prefix].toString() === 'true') {
+          entry = '<li>' + source.text + '</li>';
         }
       });
+      html += entry;
     });
 
     $(incomeLists).each(function(n, list) {
       $(list).html(html);
     });
+  },
+
+  updateThresholdFigures: function() {
+    var self = this,
+        tFloor = self.baseThresholdFloor,
+        tCeil;
+
+    if(getValue('public', 'married').toString() === 'true') {
+      tFloor += self.thresholdMarriedAllowance;
+    }
+
+    if(getValue('public', 'children')) {
+      tFloor += (parseInt(getValue('public', 'children'), 10) * self.thresholdChildAllowance);
+    }
+
+    tCeil = tFloor + self.thresholdRange;
+
+    $('.threshold-lower').text(numeral(tFloor).format('0,0'));
+    $('.threshold-upper').text(numeral(tCeil).format('0,0'));
+  },
+
+  formatCurrency: function(amount) {
+    return '£' + numeral(amount).format('0,0.00');
   }
 };
